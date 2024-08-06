64f62fcd375ac6c00f0e41434f71df4cedf2eae4
Muhammad Mudassir Zaman <127381696+zamanmudassir200@users.noreply.github.com>
2024-08-06 10:12:08 +0500

    final commit

--- array_deletion/script.js
+++ array_deletion/script.js
@@ -1,9 +1,12 @@
 const deleteBtn = document.getElementById("deleteBtn");
+let displayResult = document.getElementById("displayResult");
+const data = [4, 5, 6, 10, 100, 2, 4, 5];
+
+displayResult.textContent = data;
 
 deleteBtn.addEventListener("click", () => {
   const data = [4, 5, 6, 10, 100, 2, 4, 5];
   let position = document.getElementById("position").value;
-  const displayResult = document.getElementById("displayResult");
   console.log("Original data", data);
   position = parseInt(position);
   if (position >= 0 && position < data.length) {
