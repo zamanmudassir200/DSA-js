b5a7d857bdf112d40237ac9302f324c2ed9ce80a
Muhammad Mudassir Zaman <127381696+zamanmudassir200@users.noreply.github.com>
2024-08-08 17:59:23 +0500

    final commit

--- indirect_recursion/script.js
+++ indirect_recursion/script.js
@@ -1,19 +1,30 @@
-const buyApple = (money) => {
-  if (money > 0) {
-    console.log(
-      `I have ${money}Rs ${totalApple}:${totalApple > 1 ? "apples" : "apple"}`
-    );
-    buyMore(money);
-  } else {
-    console.log("i dont have money", money, "RS");
+// const buyApple = (money) => {
+//   if (money > 0) {
+//     console.log(
+//       `I have ${money}Rs ${totalApple}:${totalApple > 1 ? "apples" : "apple"}`
+//     );
+//     buyMore(money);
+//   } else {
+//     console.log("i dont have money", money, "RS");
+//   }
+// };
+
+// const buyMore = (money) => {
+//   totalApple++;
+//   buyApple(money - 10);
+// };
+
+// let money = 100;
+// let totalApple = 0;
+// buyApple(money);
+
+const test = (x) => {
+  // console.log(x);
+  if (x > 0) {
+    test(x - 1);
   }
+  console.log(x);
 };
 
-const buyMore = (money) => {
-  totalApple++;
-  buyApple(money - 10);
-};
-
-let money = 100;
-let totalApple = 0;
-buyApple(money);
+const data = 5;
+test(data);
