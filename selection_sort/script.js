c172877476245fa78834889ae0a536f29eaddd36
Muhammad Mudassir Zaman <127381696+zamanmudassir200@users.noreply.github.com>
2024-08-11 19:34:55 +0500

    final commit

---
+++ selection_sort/script.js
@@ -0,0 +1,19 @@
+const data = [200, 100, 50, 1, 10099, 1000, -1];
+
+const selectionSort = (data) => {
+  for (let i = 0; i < data.length; i++) {
+    let minId = i;
+    for (let j = i + 1; j < data.length; j++) {
+      if (data[j] > data[minId]) {
+        minId = j;
+      }
+    }
+    let temp = data[minId];
+    data[minId] = data[i];
+    data[i] = temp;
+    console.log(data);
+  }
+  return data;
+};
+
+selectionSort(data);
